8725a938d388c91eb9963a3bab4cabcd17e2648e
Rafaell Lycan <sonny.webdsg@gmail.com>
2019-05-01 19:29:12 +0200

    Add insert todo command

---
+++ insert_todo.js
@@ -0,0 +1,18 @@
+const client = require('./client');
+
+const title = process.argv[2];
+
+if (!title) {
+  console.error('Expected a valid title!');
+  process.exit(1);
+}
+
+client.insert({
+  title
+}, (error, todo) => {
+  if (!error) {
+    console.log('New Todo created successfully', todo)
+  } else {
+    console.error(error)
+  }
+});
